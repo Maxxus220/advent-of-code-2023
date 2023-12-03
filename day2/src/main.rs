fcd425bff2b822b6e730e291c9229d675e860042
Maxxus220 <Maxxus220@gmail.com>
2023-12-03 14:21:38 -0600

    Finished part 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,24 +1,40 @@
 use std::fs;
 
-enum Colors {
-    Red,
-    Green,
-    Blue
-}
-
 fn main() {
-    let maxes_of_colors = vec![(Colors::Red, 12), (Colors::Green, 13), (Colors::Blue, 14)];
+    let maxes_of_colors = vec![("red", 12), ("green", 13), ("blue", 14)];
 
     let file_contents = fs::read_to_string("./input").expect("File does not exist");
     let file_lines = file_contents.split("\n").into_iter().filter(|x| x.contains("Game"));
 
-    let valid_games = Vec::<u32>::new();
-    for (index, line) in file_lines.enumerate() {
-        let start_index = line.find(": ").unwrap() + 2;
-        let line = line[start_index..];
+    let mut valid_games = Vec::<u32>::new();
+    for (index, game) in file_lines.enumerate() {
+        let mut valid = true;
+
+        let start_index = game.find(": ").unwrap() + 2;
+        let line = &game[start_index..];
         let sets = line.split(";");
         for set in sets {
-            set.split(",");
+            let color_counts = set.split(",");
+            for color_count in color_counts {
+                let color_count: Vec<&str> = color_count.trim().split(" ").collect();
+                println!("Color count: {:?}", color_count);
+                for max_of_color in &maxes_of_colors {
+                    if max_of_color.0 == color_count[1] && max_of_color.1 < color_count[0].parse::<u32>().expect("Not a number") {
+                        valid = false;
+                    }
+                }
+            }
+        }
+        if valid {
+            valid_games.push((index + 1) as u32);
         }
     }
+
+    println!("Valid games: {:?}", valid_games);
+    
+    let mut sum = 0;
+    for game_indice in valid_games {
+        sum += game_indice;
+    }
+    println!("Sum of game indices: {}", sum);
 }
