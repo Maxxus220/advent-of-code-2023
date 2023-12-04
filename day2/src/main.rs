f2e0e01a85087e24956f664e4fb2af5d7b90530b
Maxxus220 <Maxxus220@gmail.com>
2023-12-03 18:47:28 -0600

    Finished day2 but it's pretty bad code

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,14 +1,13 @@
 use std::fs;
 
 fn main() {
-    let maxes_of_colors = vec![("red", 12), ("green", 13), ("blue", 14)];
 
     let file_contents = fs::read_to_string("./input").expect("File does not exist");
     let file_lines = file_contents.split("\n").into_iter().filter(|x| x.contains("Game"));
 
-    let mut valid_games = Vec::<u32>::new();
-    for (index, game) in file_lines.enumerate() {
-        let mut valid = true;
+    let mut sum = 0;
+    for game in file_lines {
+        let mut maxes_of_colors = vec![("red", 0), ("green", 0), ("blue", 0)];
 
         let start_index = game.find(": ").unwrap() + 2;
         let line = &game[start_index..];
@@ -18,23 +17,19 @@ fn main() {
             for color_count in color_counts {
                 let color_count: Vec<&str> = color_count.trim().split(" ").collect();
                 println!("Color count: {:?}", color_count);
-                for max_of_color in &maxes_of_colors {
+                for max_of_color in maxes_of_colors.iter_mut() {
                     if max_of_color.0 == color_count[1] && max_of_color.1 < color_count[0].parse::<u32>().expect("Not a number") {
-                        valid = false;
+                        max_of_color.1 = color_count[0].parse::<u32>().expect("Not a number")
                     }
                 }
             }
         }
-        if valid {
-            valid_games.push((index + 1) as u32);
+        let mut product = 1;
+        for max in maxes_of_colors {
+            product *= max.1;
         }
+        sum += product;
     }
 
-    println!("Valid games: {:?}", valid_games);
-    
-    let mut sum = 0;
-    for game_indice in valid_games {
-        sum += game_indice;
-    }
-    println!("Sum of game indices: {}", sum);
+    println!("Sum of game products: {}", sum);
 }
