013caf635bfd0726a7f836cffac3fe1a9a2a2c61
Michael Feist <michael.feist@etcconnect.com>
2023-12-05 14:53:45 -0600

    Completed day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use std::error::Error;
 use std::fs;
 
 #[derive(Debug)]
@@ -15,25 +16,43 @@ struct SymbolPosition {
 }
 
 #[derive(Debug)]
+struct SymbolInfo {
+    value: char,
+    position: SymbolPosition,
+}
+
+#[derive(Debug)]
 struct NumberInfo {
     value: u32,
-    point: NumberPosition,
+    position: NumberPosition,
 }
 
 fn main() {
-    let file_contents = fs::read_to_string("./input").expect("File does not exist");
+    let file_contents = fs::read_to_string("./day3/input").expect("File does not exist");
 
+    let (numbers, symbols) = parse_part_numbers_and_symbols(file_contents);
+
+    let sum: u32 = symbols
+        .iter()
+        .map(|symbol| get_gear_ratio(symbol, &numbers))
+        .filter_map(|gear_result| gear_result.ok())
+        .sum();
+
+    println!("The sum is {}", sum);
+}
+
+fn parse_part_numbers_and_symbols(data: String) -> (Vec<NumberInfo>, Vec<SymbolInfo>) {
     let mut numbers = Vec::<NumberInfo>::new();
-    let mut symbols = Vec::<SymbolPosition>::new();
+    let mut symbols = Vec::<SymbolInfo>::new();
 
     let mut in_num = false;
     let mut current_number: u32 = 0;
     let mut start_index: u32 = 0;
     let mut end_index: u32 = 0;
 
-    let mut line_index = 0;
-    let mut letter_index = 0;
-    for letter in file_contents.chars() {
+    let mut line_index: i32 = 0;
+    let mut letter_index: i32 = 0;
+    for letter in data.chars() {
         if letter.is_numeric() {
             current_number *= 10;
             current_number += letter.to_digit(10).unwrap();
@@ -49,7 +68,7 @@ fn main() {
 
                 let number = NumberInfo {
                     value: current_number,
-                    point: NumberPosition {
+                    position: NumberPosition {
                         start_x: start_index,
                         end_x: end_index,
                         y: line_index as u32,
@@ -69,9 +88,12 @@ fn main() {
                 line_index += 1;
                 letter_index = -1;
             } else if letter != '.' && letter != '\r' {
-                let symbol = SymbolPosition {
-                    x: letter_index as u32,
-                    y: line_index as u32,
+                let symbol = SymbolInfo {
+                    value: letter,
+                    position: SymbolPosition {
+                        x: letter_index as u32,
+                        y: line_index as u32,
+                    },
                 };
                 symbols.push(symbol);
             }
@@ -80,45 +102,47 @@ fn main() {
         letter_index += 1;
     }
 
-    // for number in &numbers {
-    //     println!("{:?}", number);
-    // }
-    // for symbol in &symbols {
-    //     println!("{:?}", symbol);
-    // }
+    (numbers, symbols)
+}
 
-    let valid_numbers: Vec<&NumberInfo> = numbers
-        .iter()
-        .filter(|number| {
-            let min_x = if number.point.start_x == 0 {
-                0
-            } else {
-                number.point.start_x - 1
-            };
-            let max_x = number.point.end_x + 1;
-            let min_y = if number.point.y == 0 {
-                0
-            } else {
-                number.point.y - 1
-            };
-            let max_y = number.point.y + 1;
-            for symbol in &symbols {
-                if symbol.x >= min_x && symbol.x <= max_x && symbol.y >= min_y && symbol.y <= max_y
-                {
-                    return true;
-                }
-            }
-            false
-        })
-        .collect();
-
-    for number in &valid_numbers {
-        println!("{:?}", number);
+fn get_gear_ratio(symbol: &SymbolInfo, numbers: &Vec<NumberInfo>) -> Result<u32, &'static str> {
+    if symbol.value != '*' {
+        return Err("Not a gear");
     }
 
-    let sum = valid_numbers
-        .iter()
-        .fold(0, |sum, number| sum + number.value);
+    let mut part_numbers = Vec::<u32>::new();
+    for number in numbers {
+        if number_is_part_of_gear(symbol, number) {
+            if part_numbers.len() >= 2 {
+                return Err("Not a gear");
+            }
 
-    println!("The solution is: {}", sum);
+            part_numbers.push(number.value);
+        }
+    }
+
+    if part_numbers.len() != 2 {
+        return Err("Not a gear");
+    }
+
+    Ok(part_numbers[0] * part_numbers[1])
+}
+
+fn number_is_part_of_gear(symbol: &SymbolInfo, number: &NumberInfo) -> bool {
+    let max_x = number.position.end_x + 1;
+    let min_x = if number.position.start_x == 0 {
+        0
+    } else {
+        number.position.start_x - 1
+    };
+    let max_y = number.position.y + 1;
+    let min_y = if number.position.y == 0 {
+        0
+    } else {
+        number.position.y - 1
+    };
+    symbol.position.x <= max_x
+        && symbol.position.x >= min_x
+        && symbol.position.y <= max_y
+        && symbol.position.y >= min_y
 }
