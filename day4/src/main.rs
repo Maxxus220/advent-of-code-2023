04798cbf728b980910f6dc4624fe3ed63351d628
Michael Feist <michael.feist@etcconnect.com>
2023-12-07 14:21:21 -0600

    Day4 part 2 completed

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,22 +1,62 @@
 use std::fs;
 
+struct CardSolution {
+    card_id: u32,
+    cards_won: Vec<u32>,
+}
+
+struct CardNumbers<'a> {
+    winning_numbers: Vec<&'a str>,
+    playing_numbers: Vec<&'a str>,
+}
+
+struct Card<'a> {
+    card_id: u32,
+    card_numbers: CardNumbers<'a>,
+}
+
 fn main() {
     let file_contents = fs::read_to_string("./day4/input").expect("File does not exist");
-    let total_card_values: u32 = file_contents
-        .lines()
-        .map(parse_cards)
-        .map(get_card_values)
-        .sum();
-    println!("The card values total to: {}", total_card_values);
+    let mut cards: Vec<Card> = file_contents.lines().map(parse_cards).collect();
+
+    let mut previous_card_solutions: Vec<CardSolution> = Vec::new();
+
+    let mut num_cards: u32 = 0;
+
+    while let Some(card) = cards.pop() {
+        num_cards += 1;
+
+        let cached_solution = previous_card_solutions
+            .iter()
+            .find(|element| element.card_id == card.card_id);
+        match cached_solution {
+            Some(solution) => {
+                push_card_solution(solution, &mut cards);
+            }
+            None => {
+                let solution = solve_card(card);
+                push_card_solution(&solution, &mut cards);
+                previous_card_solutions.push(solution);
+            }
+        }
+    }
+
+    println!("You got {} cards!", num_cards);
 }
 
 /**
- * Takes a line, removes the Card #: prefix and splits the two sides (winning_numbers, numbers)
- * into two separate Vec<&str> that are then put in a Vec<Vec<&str>> where [0] is the winning numbers
- * and [1] are the numbers. (i.e. "Card 1: 3 5 10 2 | 1 6 13 20 39 50" [0][2]=10, [1][3]=20, [0][3]=2)
+ * Takes a line and parses out the card #, winning numbers, and playing numbers.
  */
-fn parse_cards(line: &str) -> Vec<Vec<&str>> {
-    line.get(line.find(':').unwrap() + 1..)
+fn parse_cards(line: &str) -> Card {
+    let card_id = line
+        .get(4..line.find(':').unwrap())
+        .unwrap()
+        .trim()
+        .parse::<u32>()
+        .expect("Not a number");
+
+    let card_numbers = line
+        .get(line.find(':').unwrap() + 1..)
         .unwrap()
         .split('|')
         .map(|number_list| {
@@ -29,34 +69,41 @@ fn parse_cards(line: &str) -> Vec<Vec<&str>> {
                 })
                 .collect::<Vec<&str>>()
         })
-        .collect::<Vec<Vec<&str>>>()
+        .collect::<Vec<Vec<&str>>>();
+
+    Card {
+        card_id,
+        card_numbers: CardNumbers {
+            winning_numbers: card_numbers[0].clone(),
+            playing_numbers: card_numbers[1].clone(),
+        },
+    }
 }
 
 /**
- * Takes a Vec<Vec<&str>> with two Vec<&str> within. Index 0 being the winning numbers and [1] being
- * the numbers. It then calculates the score for that card based on how many numbers in the numbers list.
- * match a number in the winning numbers list. If zero numbers match then 0 otherwise the score
- * is 2^(n-1) where n is the number of numbers that match a winning number.
+ * Takes a card and figures out what cards are won by it.
  */
-fn get_card_values(card: Vec<Vec<&str>>) -> u32 {
-    let winning_nums = &card[0];
-    let nums = &card[1];
-
-    let mut num_matches = 0;
-    for num in nums {
-        if winning_nums.contains(num) {
+fn solve_card(card: Card) -> CardSolution {
+    let mut num_matches: u32 = 0;
+    for num in &card.card_numbers.playing_numbers {
+        if card.card_numbers.winning_numbers.contains(num) {
             num_matches += 1;
         }
     }
 
-    match num_matches {
-        0 => 0,
-        _ => 2_u32.pow(num_matches - 1),
+    let mut cards_won: Vec<u32> = Vec::new();
+    for card_id in card.card_id + 1..card.card_id + num_matches + 1 {
+        cards_won.push(card_id);
+    }
+
+    CardSolution {
+        card_id: card.card_id,
+        cards_won,
     }
 }
 
 /**
- * Takes a &str and returns it as a Some if not empty otherwise None
+ * Takes a &str and returns it as a Some if not empty otherwise None.
  */
 fn get_non_empty_string(string: &str) -> Option<&str> {
     if string.is_empty() {
@@ -64,4 +111,19 @@ fn get_non_empty_string(string: &str) -> Option<&str> {
     } else {
         Some(string)
     }
+}
+
+/**
+ * Pushes all won cards from a CardSolution to a Vec<Card>.
+ */
+fn push_card_solution(solution: &CardSolution, cards: &mut Vec<Card>) {
+    solution.cards_won.iter().for_each(|won_card_id| {
+        cards.push(Card {
+            card_id: *won_card_id,
+            card_numbers: CardNumbers {
+                winning_numbers: Vec::new(),
+                playing_numbers: Vec::new(),
+            },
+        })
+    });
 }
